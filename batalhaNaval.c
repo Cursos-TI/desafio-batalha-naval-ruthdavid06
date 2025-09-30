f8b76fd6ba7aef90260fece463c32fb01195b432
Ruth David <ruthcapuano1@gmail.com>
2025-09-30 03:25:26 +0000

    Nível Mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+// Desafio Jogo de Batalha Naval: nível mestre
+// Habilidades especiais e áreas de efeito
+
 // constantes que definem o tamanho da matriz (tabuleiro) e o tamanho de cada navio
 #define MATRIZ 10
 #define TAM_NAVIO 3
@@ -70,7 +73,93 @@ int main(){
     printf("Erro ao posicionar navio diagonal ↙\n");
 }
 
-    // 8. Exibe o tabuleiro com loops aninhados
+    // 8. Define o tamanho das matrizes de habilidades especiais que são representadas no tabuleiro pelo valor 5 
+    int cone[5][5] = {0};
+    int cruz[4][4] = {0};
+    int octaedro[3][3] = {0};
+
+    int inicioCone = 2; // índice 2 na matriz 5x5
+
+    for (int i = 0; i < 3; i++) { // Apenas 3 linhas serão preenchidas para formar o cone
+    int inicio = inicioCone - i; // Define o ínicio da área que será ocupada pela figura cone na matriz da habilidade especial
+    int fim = inicioCone + i; // Define o fim da área de ocupação da figura
+    for (int j = inicio; j <= fim; j++) {
+        cone[i][j] = 1; // A figura será representada pelo valor 1 na matriz de habilidade
+    }
+}
+    // Origem das coordenadas da matriz cone dentro do tabuleiro
+    int linhaTabuleiroCone = 0;
+    int colunaTabuleiroCone = 4;
+
+    for (int i = 0; i < 5; i++) { // loops aninhados para sobrepor a matriz cone ao tabuleiro
+    for (int j = 0; j < 5; j++) {
+        int linhaTab = linhaTabuleiroCone + i;
+        int colunaTab = colunaTabuleiroCone -2 + j; // desloca -2 para alinhar o cone
+
+        // Verifica se está dentro dos limites do tabuleiro
+        if (linhaTab >= 0 && linhaTab < 10 && colunaTab >= 0 && colunaTab < 10) {
+            if (cone[i][j] == 1) {
+                tabuleiro[linhaTab][colunaTab] = 5; // A figura será representada no tabuleiro pelo valor 5
+            }
+        }
+    }
+}
+
+    int centroCruz = 1; // índice 1 na matriz 4x4
+
+    for (int i = 0; i < 4; i++) { // Define as linhas que serão ocupadas pela figura na matriz da habilidade especial
+        cruz[i][centroCruz] = 1; 
+    for (int j = 0; j < 3; j++) { // Define as colunas que serão ocupadas pela figura na matriz da habilidade especial
+        cruz[centroCruz][j] = 1; // A figura será representada pelo valor 1 na matriz de habilidade 
+    }
+}
+
+    // Origem das coordenadas da matriz cruz dentro do tabuleiro
+    int linhaTabuleiroCruz = 5;
+    int colunaTabuleiroCruz = 5;
+
+    for (int i = 0; i < 4; i++){ // loops aninhados para sobrepor a matriz cruz ao tabuleiro
+    for (int j = 0; j < 4; j++){
+        int linhaTab = linhaTabuleiroCruz -1 + i;
+        int colunaTab = colunaTabuleiroCruz -1 + j;
+
+        // Verifica se está dentro dos limites do tabuleiro
+        if (linhaTab >= 0 && linhaTab < 10 && colunaTab >= 0 && colunaTab < 10){
+            if (cruz[i][j] == 1) {
+                tabuleiro[linhaTab][colunaTab] = 5; // A figura será representada no tabuleiro pelo valor 5
+            }
+        }
+    }
+}
+
+    int centroOctaedro = 1; // índice 1 na matriz 3x3
+
+    for (int i = 0; i < 3; i++) { // Define as linhas que serão ocupadas pela figura na matriz da habilidade especial
+        octaedro[i][centroOctaedro] = 1;
+    for (int j = 0; j < 3; j++) { // Define as colunas que serão ocupadas pela figura na matriz da habilidade especial
+        octaedro[centroOctaedro][j] = 1; // A figura será representada pelo valor 1 na matriz de habilidade 
+    }
+}
+
+    // Origem das coordenadas da matriz octaedro dentro do tabuleiro
+    int linhaTabuleiroOctaedro = 4;
+    int colunaTabuleiroOctaedro = 1;
+
+    for (int i = 0; i < 3; i++){ // loops aninhados para sobrepor a octaedro ao tabuleiro
+    for (int j = 0; j < 3; j++){
+        int linhaTab = linhaTabuleiroOctaedro -1 + i; 
+        int colunaTab = colunaTabuleiroOctaedro -1 + j;
+
+        // Verifica se está dentro dos limites do tabuleiro
+        if (linhaTab >= 0 && linhaTab < 10 && colunaTab >= 0 && colunaTab < 10){
+            if (octaedro[i][j] == 1){
+            tabuleiro[linhaTab][colunaTab] = 5; // A figura será representada no tabuleiro pelo valor 5
+            }
+        }
+    }
+}
+
+    // 9. Exibe o tabuleiro com loops aninhados
     printf("Tabuleiro de Batalha Naval:\n\n");
 
     for (int linha = 0; linha < 10; linha++){
